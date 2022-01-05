dd39d70bb0d914f250d3be0ae995cf305bd43fcf
Lesia Kurotchyn <lesia.kurotchyn@onix-systems.com>
2022-01-05 04:30:09 +0200

    finished video_07 rating like bookmarks

--- books/store/admin.py
+++ books/store/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from store.models import Book
+from store.models import Book, UserBookRelation
 
 
 @admin.register(Book)
 class BookAdmin(ModelAdmin):
+    pass
+
+@admin.register(UserBookRelation)
+class UserBookRelation(ModelAdmin):
     pass
--- books/store/views.py
+++ books/store/views.py
@@ -1,12 +1,13 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import UpdateModelMixin
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django.shortcuts import render
 
-from store.models import Book
+from store.models import Book, UserBookRelation
 from store.permissions import IsOwnerOrStaffOrReadOnly
-from store.serializers import BookSerializer
+from store.serializers import BookSerializer, UserBookRelationSerializer
 
 
 # http://127.0.0.1:8000/book/?format=json
@@ -22,6 +23,19 @@ class BookViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.validated_data['owner'] = self.request.user
         serializer.save()
+
+
+class UserBookRelationView(UpdateModelMixin, GenericViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = UserBookRelation.objects.all()
+    serializer_class = UserBookRelationSerializer
+    lookup_field = 'book'  # will in self.kwargs['book'] come in url instead id
+
+    def get_object(self):
+        obj, created = UserBookRelation.objects.get_or_create(user=self.request.user,
+                                                              book_id=self.kwargs['book'])
+        print('created', created)
+        return obj
 
 
 def auth(request):
